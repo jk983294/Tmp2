5b7d26b416a6967345c3f73776b1e89d8b6b16e3
jk983294 <jk983294@gmail.com>
2023-04-10 17:07:16 +0800

    init

---
+++ conf/bb/run2.sh
@@ -0,0 +1,33 @@
+rm /home/xinyu/bb
+./btc.sim -c conf/bb/conf.ini -d 20181101
+./btc.sim -c conf/bb/conf.ini -d 20181102
+./btc.sim -c conf/bb/conf.ini -d 20181103
+./btc.sim -c conf/bb/conf.ini -d 20181104
+./btc.sim -c conf/bb/conf.ini -d 20181105
+./btc.sim -c conf/bb/conf.ini -d 20181106
+./btc.sim -c conf/bb/conf.ini -d 20181107
+./btc.sim -c conf/bb/conf.ini -d 20181108
+./btc.sim -c conf/bb/conf.ini -d 20181109
+./btc.sim -c conf/bb/conf.ini -d 20181110
+./btc.sim -c conf/bb/conf.ini -d 20181111
+./btc.sim -c conf/bb/conf.ini -d 20181112
+./btc.sim -c conf/bb/conf.ini -d 20181113
+./btc.sim -c conf/bb/conf.ini -d 20181114
+./btc.sim -c conf/bb/conf.ini -d 20181115
+./btc.sim -c conf/bb/conf.ini -d 20181116
+./btc.sim -c conf/bb/conf.ini -d 20181117
+./btc.sim -c conf/bb/conf.ini -d 20181118
+./btc.sim -c conf/bb/conf.ini -d 20181119
+./btc.sim -c conf/bb/conf.ini -d 20181120
+./btc.sim -c conf/bb/conf.ini -d 20181121
+./btc.sim -c conf/bb/conf.ini -d 20181122
+./btc.sim -c conf/bb/conf.ini -d 20181123
+./btc.sim -c conf/bb/conf.ini -d 20181124
+./btc.sim -c conf/bb/conf.ini -d 20181125
+./btc.sim -c conf/bb/conf.ini -d 20181126
+./btc.sim -c conf/bb/conf.ini -d 20181127
+./btc.sim -c conf/bb/conf.ini -d 20181128
+./btc.sim -c conf/bb/conf.ini -d 20181129
+./btc.sim -c conf/bb/conf.ini -d 20181130
+
+
